ee9647e1945e2b60f47c3e27c9da2585fb64f2fc
HirokiTorigoe <106814732+HirokiTorigoe@users.noreply.github.com>
2023-02-28 14:39:44 +0900

    add flower cv website

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+document.addEventListener('DOMContentLoaded',function() {
+  const headerIcon = document.querySelector('.header__icon');
+  const attention = document.querySelector('.header__attention');
+  headerIcon.classList.add('inview');
+  attention.classList.add('inview');
+})
